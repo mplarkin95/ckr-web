97eef63cd8ef3dbdd957a80a163f64a4894efabc
Grata <mplarkin95@gmail.com>
2023-05-25 10:39:44 -0400

    local

--- src/data/transformers/artist.ts
+++ src/data/transformers/artist.ts
@@ -1,7 +1,6 @@
 import { Artist } from "@/types/Artist";
 
 export const transformArtistsFull = (apiArtists: any[]): Artist[] => {
-  console.log(apiArtists);
   return apiArtists.map((artist) => ({
     id: artist.sys.id,
     name: artist.fields.name,
