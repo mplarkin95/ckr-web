99e10f7b5b637387baca76467dc1e45d9b894c74
Grata <mplarkin95@gmail.com>
2023-05-26 17:02:52 -0400

    remove deploy

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# checkout branch main if not already on it
-if [ `git rev-parse --abbrev-ref HEAD` != "main" ]; then
-  git checkout main
-fi
-
-# push branch main to production
-git push origin main:production
