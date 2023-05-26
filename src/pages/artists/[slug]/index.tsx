5c8c1663d5814989b36041c1c57da0f82ad730bb
Grata <mplarkin95@gmail.com>
2023-05-26 18:17:49 -0400

    artist paths

--- src/pages/artists/[slug]/index.tsx
+++ src/pages/artists/[slug]/index.tsx
@@ -1,9 +1,34 @@
-const IndividualArtist = () => {
+import { getArtistBySlug, getArtists } from "@/data/queries/artists";
+import { transformArtist } from "@/data/transformers/artist";
+import { Artist } from "@/types/Artist";
+
+const IndividualArtist = ({ artist }: { artist: Artist }) => {
   return (
     <div>
-      <h1>IndividualArtist</h1>
+      <h1>{artist.name}</h1>
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  const res = await getArtists();
+  const slugs = res.map((artist) => artist.fields.slug);
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const res = await getArtistBySlug(slug);
+  const artist = transformArtist(res);
+  return {
+    props: {
+      artist,
+    },
+  };
+}
 
 export default IndividualArtist;
