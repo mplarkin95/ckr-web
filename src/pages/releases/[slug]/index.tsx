1776a99e266349292a4d4682bb74e0f6e283049e
Grata <mplarkin95@gmail.com>
2023-05-26 19:11:36 -0400

    Releases are in full swing

--- src/pages/releases/[slug]/index.tsx
+++ src/pages/releases/[slug]/index.tsx
@@ -1,9 +1,33 @@
-const Release = () => {
+import { getAlbumBySlug, getAlbums } from "@/data/queries/albums";
+import { transformAlbum } from "@/data/transformers/album";
+import { Album } from "@/types/Album";
+
+const Release = ({ album }: { album: Album }) => {
   return (
     <div>
-      <h1>Release</h1>
+      <h1>{album.name}</h1>
     </div>
   );
 };
 
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const res = await getAlbumBySlug(slug);
+  const album = transformAlbum(res);
+  return {
+    props: {
+      album,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const res = await getAlbums();
+  const slugs = res.map((album) => album.fields.slug);
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 export default Release;
