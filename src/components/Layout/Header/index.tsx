7c31d218ab4a86cf498273457a8df627b5bf1cc6
Grata <mplarkin95@gmail.com>
2023-05-30 10:26:44 -0400

    Saving place

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="fixed top-0 w-full flex h-15 border-b p-1 ">
+      <div className="flex flex-row h-full">
+        <Link href="/" className="h-full">
+          <Image
+            src={"/images/LOGOPLACEHOLDER.png"}
+            alt="logo"
+            className="h-full"
+            width={100}
+            height={100}
+          />
+        </Link>
+      </div>
+      <div className="flex flex-row justify-between w-1/6 justify-self-end items-center mr-0 ml-auto">
+        <Link className="text-lg" href="/artists">
+          Artists
+        </Link>
+        <Link className="text-lg" href="/releases">
+          Releases
+        </Link>
+        <Link className="text-lg" href="/about">
+          About
+        </Link>
+      </div>
     </header>
   );
 };
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col h-screen w-full">
       <Header />
-      <div className="container mx-auto min-h-1/2">{children}</div>
+      <div className="container mx-auto min-h-1/2 mt-20">{children}</div>
       <Footer />
     </div>
   );
