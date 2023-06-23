8efeaa11db08f7525432a509ea11af900f3d7d9a
Grata <mplarkin95@gmail.com>
2023-06-23 19:42:25 -0400

    remove element for carousel

--- src/components/Landing/Carousel.tsx
+++ src/components/Landing/Carousel.tsx
@@ -90,28 +90,6 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
           ))}
         </div>
       </div>
-      <button
-        className="absolute z-10 top-1/2 left-0 transform -translate-y-1/2 hover:bg-gray-700 rounded-full focus:outline-none"
-        onClick={handlePrevClick}
-      >
-        <svg className="w-6 h-6" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M15.41,7.42L10.83,12L15.41,16.59L14,18L8,12L14,6L15.41,7.42Z"
-          />
-        </svg>
-      </button>
-      <button
-        className="absolute z-10 top-1/2 right-0 transform -translate-y-1/2 hover:bg-gray-700 rounded-full focus:outline-none"
-        onClick={handleNextClick}
-      >
-        <svg className="w-6 h-6" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M8,7.42L9.41,6L15.41,12L9.41,18L8,16.59L12.58,12L8,7.42Z"
-          />
-        </svg>
-      </button>
     </div>
   );
 };
