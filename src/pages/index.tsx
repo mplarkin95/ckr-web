4a40477fa84c9da83c6dbcc39cc6eee3f5e781c9
Grata <mplarkin95@gmail.com>
2023-06-28 23:09:19 -0400

    start of splash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 
 const Index = () => {
-  const foo = [...Array(1000)].map((_, i) => <div key={i}>{i}</div>);
-  return <div className="flex flex-col">{foo.map((entry) => entry)}</div>;
+  return <div className="flex flex-col">logo, sg</div>;
 };
 
 export default Index;
