22aeb02190ae793f90e2c7a94e34e2bb76ad85e3
Grata <mplarkin95@gmail.com>
2023-07-26 23:53:45 -0400

    release skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-const Index = () => {
-  return <div>This is content</div>;
+import { getReleases } from "@/data/queries/releases";
+import { transformReleasesFull } from "@/data/transformers/release";
+import { Release } from "@/types/Release";
+
+const Index = ({ releases }: { releases: Release[] }) => {
+  return (
+    <div>
+      <h1>Releases</h1>
+      {releases.map((release) => (
+        <h2 key={release.id}>{release.title}</h2>
+      ))}
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  const res = await getReleases();
+  const releases = transformReleasesFull(res);
+  return {
+    props: {
+      releases,
+    },
+  };
+}
 
 export default Index;
