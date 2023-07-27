0c703fa98912ba25a4ca449ca9af4c15d4f75da8
Grata <mplarkin95@gmail.com>
2023-07-26 23:58:27 -0400

    Markdown renderer working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { getReleases } from "@/data/queries/releases";
 import { transformReleasesFull } from "@/data/transformers/release";
 import { Release } from "@/types/Release";
+import Markdown from "marked-react";
 
 const Index = ({ releases }: { releases: Release[] }) => {
   return (
     <div>
       <h1>Releases</h1>
       {releases.map((release) => (
-        <h2 key={release.id}>{release.title}</h2>
+        <>
+          <h2 key={release.id}>{release.title}</h2>
+          <Markdown>{release.copywrite}</Markdown>
+        </>
       ))}
     </div>
   );
