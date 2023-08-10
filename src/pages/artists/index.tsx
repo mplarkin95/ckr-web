cfaec65b46492ac2c2544e41884a4765fb731d6d
Grata <mplarkin95@gmail.com>
2023-08-09 22:14:49 -0400

    artist page

--- src/pages/artists/index.tsx
+++ src/pages/artists/index.tsx
@@ -7,11 +7,25 @@ const AllArtist = ({ artists }: { artists: Artist[] }) => {
   return (
     <div>
       <h1>Artists</h1>
-      {artists.map((artist) => (
-        <Link key={artist.id} href={`/artists/${artist.slug}`}>
-          <h2>{artist.name}</h2>
-        </Link>
-      ))}
+      <div className="flex flex-row w-full flex-wrap justify-between">
+        {artists.map((artist) => (
+          <Link
+            key={artist.id}
+            href={`/artists/${artist.slug}`}
+            className="lg:w-1/3 w-1/2 flex justify-center mx-auto "
+          >
+            <div className="flex flex-col justify-center items-center ">
+              <img
+                src={`https:${artist.profilePicture}`}
+                className="rounded-full w-32 h-32 lg:w-64 lg:h-64 object-cover"
+              />
+              <div className="flex flex-row justify-center py-3 border-t-2 border-b-2 border-gray-500 my-2 mb-8">
+                {artist.name}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
